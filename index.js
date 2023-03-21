35f5146b8139b9ae306100f1474409a1e28c2cba
Inda Smith <inda.k.smith@gmail.com>
2023-03-21 17:26:30 -0400

    finally got the tests to pass. took us 3 hours. damnit.

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback) {
+    const Spy = prompt ("We're all in this together.")
+    callback(Spy);
+}
+
+function returnsANamedFunction() {
+      return returnsANamedFunction
+
+}
+
+
+
+
+function returnsAnAnonymousFunction() {
+
+    return () => console.log('Hi everybody!')
+}
